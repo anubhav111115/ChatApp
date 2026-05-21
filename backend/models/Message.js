42177df83b66f372e51a09fc5bbd02258cbd9fb0
ANUBHAV SINGH <anubhavsingh11112005@gmail.com>
2026-05-21 11:59:37 +0530

    Fixed theattachment issue

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -2,17 +2,17 @@ const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema(
   {
-    sender: { type: String, required: true },
+    sender:  { type: String, required: true },
     message: { type: String, default: "" },
-    room: { type: String, required: true, default: "general" },
+    room:    { type: String, required: true, default: "general" },
     fileData: {
-      name: String,
-      size: Number,
-      type: String,
-      data: String,
-      isImage: Boolean,
+      name:    { type: String },
+      size:    { type: Number },
+      type:    { type: String },
+      data:    { type: String },
+      isImage: { type: Boolean },
     },
-    edited: { type: Boolean, default: false },
+    edited:  { type: Boolean, default: false },
     deleted: { type: Boolean, default: false },
   },
   { timestamps: true }
